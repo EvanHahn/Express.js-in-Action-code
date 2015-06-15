35a9487478ebe2953388e1058b9bbf5a8a29f3ff
Evan Hahn <me@evanhahn.com>
2015-06-15 08:31:42 -0700

    Chapter 8 bugfix

--- Chapter_08/learn-about-me/routes.js
+++ Chapter_08/learn-about-me/routes.js
@@ -90,7 +90,10 @@ router.post("/edit", ensureAuthenticated, function(req, res, next) {
   req.user.displayName = req.body.displayname;
   req.user.bio = req.body.bio;
   req.user.save(function(err) {
-    if (err) { next(err); }
+    if (err) {
+      next(err);
+      return;
+    }
     req.flash("info", "Profile updated!");
     res.redirect("/edit");
   });
