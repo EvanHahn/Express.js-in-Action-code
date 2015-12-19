5272bd80b756ee80d689d8767c8cf7421f1fedfe
Evan Hahn <me@evanhahn.com>
2015-12-19 11:56:31 -0800

    Update Chapter 4 example; drop fs.exists

--- Chapter_04/static-file-fun/first-version.js
+++ Chapter_04/static-file-fun/first-version.js
@@ -1,1 +1,36 @@
-var express = require("express");var path = require("path");var fs = require("fs");var app = express();app.use(function(req, res, next) {  console.log("Request IP: " + req.url);  console.log("Request date: " + new Date());  next();});app.use(function(req, res, next) {  var filePath = path.join(__dirname, "static", req.url);  fs.exists(filePath, function(exists) {    if (exists) {      res.sendFile(filePath);    } else {      next();    }  });});app.use(function(req, res) {  res.status(404);  res.send("File not found!");});app.listen(3000, function() {  console.log("App started on port 3000");});
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+var app = express();
+
+app.use(function(req, res, next) {
+  console.log("Request IP: " + req.url);
+  console.log("Request date: " + new Date());
+  next();
+});
+
+app.use(function(req, res, next) {
+  var filePath = path.join(__dirname, "static", req.url);
+  fs.stat(filePath, function(err, fileInfo) {
+    if (err) {
+      next();
+      return;
+    }
+
+    if (fileInfo.isFile()) {
+      res.sendFile(filePath);
+    } else {
+      next();
+    }
+  });
+});
+
+app.use(function(req, res) {
+  res.status(404);
+  res.send("File not found!");
+});
+
+app.listen(3000, function() {
+  console.log("App started on port 3000");
+});
