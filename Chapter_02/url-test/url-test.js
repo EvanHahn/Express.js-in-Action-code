43e5ddbcd6f5b947050af245b8616b726562c4b2
Michael Hoffman <mikeshoffman@gmail.com>
2018-03-22 11:39:01 -0700

    Remove ch2 directories that have 1 file

--- Chapter_02/url-test/url-test.js
+++
@@ -1,6 +0,0 @@
-var url = require("url");
-var parsedURL = url.parse("http://www.example.com/profile?name=barry");
-
-console.log(parsedURL.protocol); // "http:"
-console.log(parsedURL.host); // "www.examplecom"
-console.log(parsedURL.query); // "name=barry"
